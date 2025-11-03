19c3b4a4ce5fcb2734f94d21e20599700b53c984
killpopmusic <tomek.talarczyk@gmail.com>
2025-11-03 13:37:26 +0100

    1st model architecture for testing

---
+++ src/model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+#First model 3 hidden layers with ReLU
+
+class TestModel(nn.Module):
+    
+    def __init__(self, input_features, output_features=2):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_features, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, output_features)
+        )
+
+    def forward(self, x):
+        return self.model(x)
