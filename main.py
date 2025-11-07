b0932119d0dadc1e7966a079ce2f0a1febf5a73a
killpopmusic <tomek.talarczyk@gmail.com>
2025-11-07 13:16:37 +0100

    adjust main.py

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ import torch
 
 from src.train import train_model
 from src.evaluate import evaluate_model
-from src.utils.data_loader import get_data_loaders
+from src.utils.h5_data_loader import get_h5_data_loaders
 
 def main():
     parser = argparse.ArgumentParser(description="Gaze Tracking Model Training and Evaluation")
     parser.add_argument('--mode', type=str, default='train', choices=['train', 'evaluate'], help='Mode to run the script in.')
     parser.add_argument('--model_name', type=str, default='TestModel', help='Name of the model class to use.')
-    parser.add_argument('--data_path', type=str, default='data/merged_dataset.json', help='Path to the training/evaluation data.')
-    parser.add_argument('--epochs', type=int, default=200, help='Number of training epochs.')
+    parser.add_argument('--data_path', type=str, default='data/HybridGaze.h5', help='Path to the training/evaluation data.')
+    parser.add_argument('--epochs', type=int, default=2000, help='Number of training epochs.')
     parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training and validation.')
     parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate for the optimizer.')
     parser.add_argument('--model_path', type=str, default='models/gaze_model.pth', help='Path to save or load the model.')
+    parser.add_argument('--num_persons', type=int, default=10, help='Number of persons to use for training/evaluation.')
 
     args = parser.parse_args()
 
@@ -31,7 +32,7 @@ def main():
 
     if args.mode == 'train':
         print(f"Starting training on {device}...")
-        train_loader, val_loader, input_features = get_data_loaders(data_path=args.data_path, batch_size=args.batch_size)
+        train_loader, val_loader, input_features = get_h5_data_loaders(data_path=args.data_path, batch_size=args.batch_size, num_persons=args.num_persons)
         model = model_class(input_features).to(device)
         
         train_model(model, train_loader, val_loader, num_epochs=args.epochs, lr=args.lr, model_path=args.model_path)
@@ -52,10 +53,12 @@ def main():
         model = model_class(checkpoint['input_features']).to(device)
         model.load_state_dict(checkpoint['model_state_dict'])
 
-        _, val_loader, _ = get_data_loaders(data_path=args.data_path, batch_size=args.batch_size)
+        _, val_loader, _ = get_h5_data_loaders(data_path=args.data_path, batch_size=args.batch_size, num_persons=args.num_persons)
         
         evaluate_model(model, val_loader, args.model_path)
         print("Evaluation finished.")
 
 if __name__ == '__main__':
     main()
+
+
