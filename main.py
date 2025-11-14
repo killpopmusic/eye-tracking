473f48363ffab15cc9d7f4584e373722ea4c5360
killpopmusic <tomek.talarczyk@gmail.com>
2025-11-14 19:02:33 +0100

    manual id selection + use of 2nd model

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ from src.utils.h5_data_loader import get_h5_data_loaders
 def main():
     parser = argparse.ArgumentParser(description="Gaze Tracking Model Training and Evaluation")
     parser.add_argument('--mode', type=str, default='train', choices=['train', 'evaluate'], help='Mode to run the script in.')
-    parser.add_argument('--model_name', type=str, default='TestModel', help='Name of the model class to use.')
+    parser.add_argument('--model_name', type=str, default='TestModel2', help='Name of the model class to use.')
     parser.add_argument('--data_path', type=str, default='data/HybridGaze.h5', help='Path to the training/evaluation data.')
-    parser.add_argument('--epochs', type=int, default=1500, help='Number of training epochs.')
-    parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training and validation.')
-    parser.add_argument('--lr', type=float, default=0.0002, help='Learning rate for the optimizer.')
+    parser.add_argument('--epochs', type=int, default=500, help='Number of training epochs.')
+    parser.add_argument('--batch_size', type=int, default=64, help='Batch size for training and validation.')
+    parser.add_argument('--lr', type=float, default=0.0005, help='Learning rate for the optimizer.')
     parser.add_argument('--model_path', type=str, default='models/gaze_model.pth', help='Path to save or load the model.')
 
     args = parser.parse_args()
@@ -25,6 +25,9 @@ def main():
     if args.model_name == 'TestModel':
         from src.models.test_model import TestModel
         model_class = TestModel
+    elif args.model_name == 'TestModel2':
+        from src.models.test_model_v2 import TestModelV2
+        model_class = TestModelV2
     else:
         print(f"Model {args.model_name} not found.")
         return
@@ -36,19 +39,19 @@ def main():
         all_person_ids = np.unique([pid.decode('utf-8') for pid in f['data']['person_id'][:]])
 
     # Split person IDs for training/validation and testing
-    train_person_ids = all_person_ids[:13]
-    test_person_ids = all_person_ids[13:]
+    train_person_ids = all_person_ids[[0,2,4,5,6,7,8,9,10,11,12,13,14]]  # without 0, 2, 5, 6, 7, 12, 11
+    test_person_ids = all_person_ids[[1,3]]
     print(f"Training/Validation on {len(train_person_ids)} persons: {train_person_ids}")
     print(f"Testing on {len(test_person_ids)} persons: {test_person_ids}")
 
     hyperparameters = {
         "epochs": args.epochs,
         "batch_size": args.batch_size,
-        "learning_rate": args.lr
+        "learning_rate": args.lr,
+        "target": "gaze"
     }
 
     if args.mode == 'train':
-        print(f"Starting training on {device}...")
         train_loader, val_loader, test_loader, input_features, source_csv_test, y_test, gaze_test, person_ids_test = get_h5_data_loaders(
             data_path=args.data_path, 
             batch_size=args.batch_size, 
@@ -61,15 +64,11 @@ def main():
         
         print("Training finished. Starting evaluation on the test set...")
         evaluate_model(model, test_loader, args.model_path, source_csv_test, y_test, gaze_test, person_ids_test, hyperparameters)
-        print("Evaluation finished.")
 
     elif args.mode == 'evaluate':
         if not os.path.exists(args.model_path):
             print(f"Model file not found at {args.model_path}")
             return
-            
-        print(f"Starting evaluation on {device}...")
-        
         _, _, test_loader, input_features, source_csv_test, y_test, gaze_test, person_ids_test = get_h5_data_loaders(
             data_path=args.data_path, 
             batch_size=args.batch_size,
@@ -80,7 +79,6 @@ def main():
         model = model_class(input_features).to(device)
         
         evaluate_model(model, test_loader, args.model_path, source_csv_test, y_test, gaze_test, person_ids_test, hyperparameters)
-        print("Evaluation finished.")
 
 if __name__ == '__main__':
     main()
